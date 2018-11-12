adca61101efdd893cfa3d3c0f5a5de4e19669636
André <andrerrr@outlook.de>
2018-11-12 14:12:17 +0100

    Hallo Builder und Singelton

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace HalloBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Schrank meinSchrank = new Schrank.SchrankBuilder().SetAnzahlTüren(5).SetOberfläche(Oberfläche.Lackiert).SetFarbe("Rot").Create();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Schrank
+    {
+        public int AnzahlTüren { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+        public string Farbe { get; private   set; }
+        public int Böden { get; private set; }
+        public bool Stange { get; private set; }
+
+        public class SchrankBuilder
+        {
+            private Schrank result = new Schrank();
+
+            public SchrankBuilder SetAnzahlTüren(int zahl)
+            {
+                if (zahl < 2 || zahl > 10)
+                    throw new ArgumentException("Zu viele oder zu wenig Türen");
+
+                result.AnzahlTüren = zahl;
+                return this;
+            }
+
+            public SchrankBuilder SetFarbe(string farbe)
+            {
+                if (result.Oberfläche != Oberfläche.Lackiert)
+                    throw new ArgumentException("Nur lackierte Schränke können eine Farbe haben");
+
+                result.Farbe = farbe;
+                return this;
+            }
+
+            public SchrankBuilder SetOberfläche(Oberfläche of)
+            {
+                result.Oberfläche = of;
+                return this;
+            }
+            public Schrank Create()
+            {
+                return result;
+            }
+        }
+    }
+
+    enum Oberfläche
+    {
+        Unbehandelt,
+        Gewachst,
+        Lackiert
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Parallel.For(0, 10, i => Logger.Instance.Log($"Hallo {i}"));
+
+            Console.ReadLine();
+        }
+    }
+
+    class Logger
+    {
+        private static object _syncObj = new object();
+        private static Logger _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_syncObj)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+        static int instCounter = 0;
+        private Logger()
+        {
+            instCounter++;
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{instCounter}] [{DateTime.Now:T}] {msg}");
+        }
+    }
+}
