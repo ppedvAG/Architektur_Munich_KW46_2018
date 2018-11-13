fdcf2f40cb0ccbea2549cf824ddd1f91cd49e255
André <andrerrr@outlook.de>
2018-11-13 14:50:41 +0100

    Repository implementiert

---
+++ ppedv.Zeus/ppedv.Zeus.Data.EF/EfRepository.cs
@@ -0,0 +1,47 @@
+﻿using ppedv.Zeus.Model;
+using ppedv.Zeus.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.Zeus.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        private EfContext context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Drucker))
+            //    context.Drucker.Add(entity as Drucker);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ ppedv.Zeus/ppedv.Zeus.Logic/Core.cs
@@ -0,0 +1,19 @@
+﻿using ppedv.Zeus.Model.Contracts;
+using System;
+
+namespace ppedv.Zeus.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repository)
+        {
+            this.Repository = repository;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+
+    }
+}
