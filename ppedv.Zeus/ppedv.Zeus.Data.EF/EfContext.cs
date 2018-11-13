eedae1e71909966c7b944514d8337285a64b9eac
André <andrerrr@outlook.de>
2018-11-13 11:37:16 +0100

    Ef konfiguriert

---
+++ ppedv.Zeus/ppedv.Zeus.Data.EF/EfContext.cs
@@ -0,0 +1,12 @@
+﻿using ppedv.Zeus.Model;
+using System.Data.Entity;
+
+namespace ppedv.Zeus.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Drucker> Drucker { get; set; }
+        public DbSet<Auftrag> Auftraege { get; set; }
+        public DbSet<Modell> Modelle { get; set; }
+    }
+}
